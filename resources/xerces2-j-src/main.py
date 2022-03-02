2d1c276c81b5ec52b0decb0345546117f13f0d5f
filippocasari <fili9941@gmail.com>
2022-03-02 17:41:19 +0100

    third commit

--- resources/xerces2-j-src/main.py
+++ resources/xerces2-j-src/main.py
@@ -4,12 +4,9 @@ import os
 import pandas as pd
 d={'class_name':[], 'methods_num':[]}
 dataframe= pd.DataFrame(data=d)
-print(dataframe)
-
-
-
+import time
+start=time.time()
 for root, dirs, files in os.walk(".", topdown=False):
-    
     
     for name in files:
       
@@ -19,7 +16,6 @@ for root, dirs, files in os.walk(".", topdown=False):
         data = open(nome).read()
         tree_tmp=javalang.parse.parse(data)
         
-        numbers_of_methods=0
         for klass in tree_tmp.types:
             if isinstance(klass, javalang.tree.ClassDeclaration) and klass.name is not d['class_name']:
                 name_class=klass.name
@@ -34,8 +30,13 @@ std_num_methods=dataframe['methods_num'].std()
 print("mean of methods= ", dataframe['methods_num'].mean())
 print("std of methods= ", dataframe['methods_num'].std())
 
-names=dataframe.loc[dataframe['methods_num']>(mean_num_methods+6*(std_num_methods))]
-print(names)
+names_god=dataframe.loc[dataframe['methods_num']>(mean_num_methods+6*(std_num_methods))]
+
+#print(names)
+print("the God classes are: ")
+for i in names_god["class_name"]:
+    print(i, end='\n')
+print("time of execution: ", time.time()-start, " seconds")
 
     
 
