438c13df7235f692fa251d240f34845242e8df09
filippocasari <fili9941@gmail.com>
2022-03-30 10:00:10 +0200

    changing names with underscores

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from lab1 import return_output_first_step
-from lab2 import second_step, write_csv
+from find_god_classes import return_output_first_step
+from extract_feature_vectors import second_step, write_csv
 
 god_class_names_final=return_output_first_step()
 
